556055b4ef885ef7d5a1aea3f8f971cab693c796
Peter Durham <peterdurham@protonmail.com>
2021-08-22 09:13:55 -0700

    added todo note

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
-const request = require('request');
-const macaroon = fs.readFileSync('/home/umbrel/umbrel/lnd/data/chain/bitcoin/mainnet/admin.macaroon').toString('hex');
+const fs = require("fs");
+const request = require("request");
+const macaroon = fs
+  .readFileSync(
+    "/home/umbrel/umbrel/lnd/data/chain/bitcoin/mainnet/admin.macaroon"
+  )
+  .toString("hex");
 
 console.log(macaroon);
 
 let options = {
-  url: 'https://localhost:8080/v1/channels',
+  url: "https://localhost:8080/v1/channels",
   // Work-around for self-signed certificates.
   rejectUnauthorized: false,
   json: true,
   headers: {
-    'Grpc-Metadata-macaroon': macaroon,
+    "Grpc-Metadata-macaroon": macaroon,
   },
-}
-request.get(options, function(error, response, body) {
+};
+request.get(options, function (error, response, body) {
   console.log(options, error, response, body);
 });
 
 console.log("hi there");
+
+// TODO: set function for get channels
