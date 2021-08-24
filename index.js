7b8325d8cb35b01c59f1d16b7a25da5f981b9acc
Peter Durham <peterdurham@protonmail.com>
2021-08-24 03:14:47 +0000

    hooked up async await on functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const axios = require("axios");
 const request = require("request");
 const macaroon = fs
   .readFileSync(
@@ -6,7 +7,16 @@ const macaroon = fs
   )
   .toString("hex");
 
-console.log(macaroon);
+async function asyncGetRequest(options) {
+  return new Promise((resolve, reject) => {
+    request.get(options, (error, response, body) => resolve({ error, response, body}));
+  });
+}
+async function asyncPostRequest(options) {
+  return new Promise((resolve, reject) => {
+    request.post(options, (error, response, body) => resolve({ error, response, body }));
+  });
+}
 
 let options = {
   url: "https://localhost:8080/v1/channels",
@@ -17,10 +27,38 @@ let options = {
     "Grpc-Metadata-macaroon": macaroon,
   },
 };
-request.get(options, function (error, response, body) {
-  console.log(options, error, response, body);
-});
 
-console.log("hi there");
+function getOptions(url, requestBody = null) {
+  let options = {
+    url: url,
+    rejectUnauthorized: false,
+    json: true,
+    headers: {
+      "Grpc-Metadata-macaroon": macaroon,
+    }
+  }
+  if (requestBody) {
+    options.form = JSON.stringify(requestBody);
+  }
+console.log(options);
+  return options;
+}
+
+async function getChannels() {
+  let res = await asyncGetRequest(getOptions("https://localhost:8080/v1/channels"));
+  console.log(res.body);
+}
+getChannels();
+
+
+async function getRoutes(pubkey, amount, channelID) {
+  let requestBody = {
+    outgoing_chan_id: channelID,
+  }
+  let res = await asyncGetRequest(getOptions(`https://localhost:8080/v1/graph/routes/${pubkey}/${amount}`, requestBody));
+  console.log(res)
+  console.log(res.body.routes[0].hops);
+}
+//getRoutes('03e94e144ad749a1cb11972212267fac12c0abfe0ca5a33985dcece556e62a1443', 10000, '762809281549762560');
 
 // TODO: set function for get channels
