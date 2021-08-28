08c10251c8989b430e37e610f7489724b28f6257
Peter Durham <peterdurham@protonmail.com>
2021-08-28 10:57:48 -0700

    added fee lookup

--- fees.js
+++ fees.js
@@ -5,8 +5,18 @@ async function getChannelFees() {
   let chanResp = await asyncGetRequest(
     getOptions("https://localhost:8080/v1/channels")
   );
-  let feeResp = await asyncGetRequest(getOptions("https://localhost:8080/v1/fees"));
+  let feeResp = await asyncGetRequest(
+    getOptions("https://localhost:8080/v1/fees")
+  );
   console.log(chanResp.body.channels);
   console.log(feeResp.body);
+  const channels = chanResp.body.channels;
+
+  for (let i = 0; i < channels.length; i++) {
+    const res = await asyncGetRequest(
+      getOptions(`https://localhost:8080/v1/graph/edge/${channels[i].chan_id}`)
+    );
+    console.log(res.body);
+  }
 }
 getChannelFees();
